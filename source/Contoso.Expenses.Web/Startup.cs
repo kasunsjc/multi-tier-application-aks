0611a86c59653aca712d42f78886cc64cab03a34
Kasun Rajapakse <kasunsjc@hotmail.com>
2021-03-07 17:36:51 +0530

    inetial commit

---
+++ source/Contoso.Expenses.Web/Startup.cs
@@ -0,0 +1,107 @@
+﻿using Contoso.Expenses.Common.Models;
+using Contoso.Expenses.Web.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+
+namespace Contoso.Expenses.Web
+{
+    public class Startup
+    {
+        private readonly IWebHostEnvironment _env;
+        public IConfiguration Configuration { get; }
+
+        public Startup(IWebHostEnvironment env)
+        {
+            IConfigurationBuilder builder = new ConfigurationBuilder()
+                            .SetBasePath(env.ContentRootPath)
+                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                            .AddEnvironmentVariables();
+
+            if (env.IsDevelopment())
+            {
+                builder.AddUserSecrets<Startup>();
+            }
+
+            _env = env;
+
+            Configuration = builder.Build();
+            //Configuration = configuration;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<KestrelServerOptions>(options => { options.AllowSynchronousIO = true; });
+
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+
+            string keyVaultName = Configuration["KeyVaultName"];
+            var kvUri = "https://" + keyVaultName + ".vault.azure.net";
+            var client = new SecretClient(new Uri(kvUri), new DefaultAzureCredential());
+            var secretDbConn = client.GetSecret("mysqlconnweb");
+            var secretStorageConn = client.GetSecret("storageconn");
+            string connectionDBString = secretDbConn.Value.Value;
+            string connectionStorageString = secretStorageConn.Value.Value;
+            services.AddDbContext<ContosoExpensesWebContext>(options =>
+                    options.UseMySql(connectionDBString));
+
+            services.Configure<ConfigValues>(Configuration.GetSection("ConfigValues"));
+
+            services.AddSingleton<QueueInfo>(queueInfo =>
+            {
+                return new QueueInfo()
+                {
+                    ConnectionString = connectionStorageString,
+                    QueueName = Configuration["QueueName"]
+                };
+            });
+
+            services.AddSingleton<IWebHostEnvironment>(_env);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ContosoExpensesWebContext context)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            context.Database.EnsureCreated();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapRazorPages();
+            });
+        }
+    }
+}
